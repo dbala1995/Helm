694d9e158fface60428866870436dcb014d84662
Dan Bala <dan.bala@synanetics.com>
2021-07-22 15:27:21 +0100

    fixed final clash

--- library/src/components/CustomElements/QuestionnaireComponent.js
+++ library/src/components/CustomElements/QuestionnaireComponent.js
@@ -13,14 +13,6 @@ class QuestionnaireComponent extends ReactMaterialComponentBase {
         this.jsxRootComponent = () => {
             const questionnaireList = this.resources.Questionnaire
             const top3ThingsQuestionnaire = this.resources.top3ThingsQuestionnaire
-<<<<<<< HEAD
-            return <Provider store={store} >
-                <Questionnaire
-                    resources={this.resources}
-                    submit={(changedResource) => this.submit([changedResource])}
-                    requestResources={(questionResponse, queryParams, bodyParams) => this.requestResources(questionResponse, queryParams, bodyParams)} />
-            </Provider >
-=======
             return (
                 <Provider store={store}>
                     <Questionnaire
@@ -32,11 +24,10 @@ class QuestionnaireComponent extends ReactMaterialComponentBase {
                     />
                 </Provider>
             )
->>>>>>> 168031cc1d8b5cc774dc6baf88e8317ce7f18869
         }
     }
 
-    getTop3Things() {}
+    getTop3Things() { }
 }
 
 customElements.define(
